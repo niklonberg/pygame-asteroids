d06e19d0f0516ad0da718d3ec1d98926e5fb1153
Nikolai Lønberg <niklonberg@gmail.com>
2025-01-17 12:59:04 +0100

    setup screen size and initialize game engine

--- main.py
+++ main.py
@@ -6,5 +6,21 @@ def main():
   print(f"Screen width: {SCREEN_WIDTH}")
   print(f"Screen height: {SCREEN_HEIGHT}")
 
+  # Initialize pygame engine/library
+  pygame.init()
+
+  # Create game window with specified dimensions
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # setups screen
+
+  while True:
+    for event in pygame.event.get():
+      # Handle window close button event
+      if event.type == pygame.QUIT:
+        return
+      
+    screen.fill("black")
+    pygame.display.flip()
+
+
 if __name__ == "__main__":
   main()
