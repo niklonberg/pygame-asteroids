a46b2343be30154208b860c49ce0bb89f11922ad
Nikolai Lønberg <niklonberg@gmail.com>
2025-01-18 14:52:08 +0100

    add deltatime clock

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
 
   # Initialize pygame engine/library
   pygame.init()
+  clock = pygame.time.Clock()
+  dt = 0
 
   # Create game window with specified dimensions
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # setups screen
@@ -17,9 +19,10 @@ def main():
       # Handle window close button event
       if event.type == pygame.QUIT:
         return
-      
+    
     screen.fill("black")
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
