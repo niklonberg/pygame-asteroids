926fc8d186b418e1b7f8f52e8dcadf33b74454f2
Nikolai Lønberg <niklonberg@gmail.com>
2025-01-20 14:58:02 +0100

    add asteroids to game loop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
   print("Starting asteroids!")
@@ -19,9 +21,13 @@ def main():
   updateable = pygame.sprite.Group()
   drawable = pygame.sprite.Group()
   Player.containers = (updateable, drawable)
+  asteroids = pygame.sprite.Group()
+  Asteroid.containers = (asteroids, updateable, drawable)
+  AsteroidField.containers = updateable
 
   # Create player
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+  asteroids = AsteroidField()
 
   while True:
     for event in pygame.event.get():
